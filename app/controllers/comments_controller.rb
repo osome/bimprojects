adc0d6054e16871d2e174daa23fb7cbd05059147
Kyle Morin <kyle.t.morin@gmail.com>
2013-06-07 11:34:06 -0700

    fixing minor issues

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -43,10 +43,11 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(params[:comment])
+    @project = Project.find(@comment.project_id)
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @project, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
